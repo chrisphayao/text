404240beb76ea8b0a409b19a415fbf651bb3dc7b
chrisphayao <135191389+chrisphayao@users.noreply.github.com>
2023-11-27 19:44:33 +0700

    Add files via upload

---
+++ text-summarizer.py
@@ -0,0 +1,75 @@
+import streamlit as st
+from txtai.pipeline import Summary, Textractor
+from PyPDF2 import PdfReader
+
+st.set_page_config(layout="wide")
+
+@st.cache_resource
+def text_summary(text,minL=None,maxL=None):
+    #create summary instance
+    summary = Summary()
+    text = (text)
+    result = summary(text, minlength=minL, maxlength=maxL)
+    # result = summary(text)
+    return result
+
+def extract_text_from_pdf(file_path):
+    # Open the PDF file using PyPDF2
+    with open(file_path, "rb") as f:
+        reader = PdfReader(f)
+        page = reader.pages[0]
+        text_p1 = page.extract_text()
+        # Initialize an empty string to store concatenated text
+        all_text = ""
+
+       # Loop through all pages and extract text
+        for page_number in range(len(reader.pages)):
+           page = reader.pages[page_number]
+           all_text += page.extract_text()
+    return text_p1,all_text
+
+def countWords(text):
+    # Split the text into words
+    words = text.split()
+    # Count the number of words
+    return len(words)
+
+
+choice = st.sidebar.selectbox("Select your choice", ["Summarize Text", "Summarize Document"])
+
+if choice == "Summarize Text":
+    st.subheader("Summarize Text using txtai")
+    input_text = st.text_area("Enter your text here")
+    if input_text is not None:
+        if st.button("Summarize Text"):
+            col1, col2 = st.columns([1,1])
+            with col1:
+                st.markdown("**Your Input Text**")
+                st.info(input_text)
+            with col2:
+                st.markdown("**Summary Result**")
+                result = text_summary(input_text)
+                st.success(result)
+
+elif choice == "Summarize Document":
+    st.subheader("Summarize Document using txtai")
+    input_file = st.file_uploader("Upload your document here", type=['pdf'])
+    if input_file is not None:
+        if st.button("Summarize Document"):
+            with open("doc_file.pdf", "wb") as f:
+                f.write(input_file.getbuffer())
+            col1, col2 = st.columns([1,1])
+            with col1:
+                st.info("File uploaded successfully")
+                text1,extracted_text = extract_text_from_pdf("doc_file.pdf")
+                word_count = countWords(extracted_text)
+
+                st.markdown("**Extracted Text: "+str(word_count)+" words**")
+                # show only first page
+                st.info(text1)
+            with col2:
+                st.markdown("**Summary Result**")
+                text = extract_text_from_pdf("doc_file.pdf")
+                doc_summary = text_summary(text,int(word_count/20),int(word_count/4))
+                st.markdown(str(len(doc_summary))+" words in summary")
+                st.success(doc_summary)
